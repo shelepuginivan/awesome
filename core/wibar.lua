4430de3aae5b986757e715134ad6f96c40854cfe
shelepuginivan <shelepuginivanm@gmail.com>
2023-12-02 00:32:35 +0300

    feat(core): separate wibar configuration

---
+++ core/wibar.lua
@@ -0,0 +1,74 @@
+local awful = require('awful')
+local gears = require('gears')
+local wibox = require('wibox')
+
+local wibar = require('theme.wibar')
+local wallpaper = require('theme.wallpaper')
+
+local function on_connect(s)
+    -- Wallpaper
+    wallpaper.set(s)
+
+    -- Each screen has its own tag table.
+    local names = { 'term', 'www', 'office', 'media' }
+    awful.tag(names, s, awful.layout.layouts[1])
+
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(gears.table.join(
+        awful.button({}, 1, function()
+            awful.layout.inc(1)
+        end),
+        awful.button({}, 3, function()
+            awful.layout.inc(-1)
+        end),
+        awful.button({}, 4, function()
+            awful.layout.inc(1)
+        end),
+        awful.button({}, 5, function()
+            awful.layout.inc(-1)
+        end)
+    ))
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist({
+        screen = s,
+        filter = awful.widget.taglist.filter.all,
+        buttons = wibar.buttons.taglist,
+    })
+
+    -- Create a tasklist widget
+    s.mytasklist = awful.widget.tasklist({
+        screen = s,
+        filter = awful.widget.tasklist.filter.currenttags,
+        buttons = wibar.buttons.tasklist,
+    })
+
+    -- Create the wibox
+    s.mywibox = awful.wibar({ position = 'top', screen = s })
+
+    -- Add widgets to the wibox
+    s.mywibox:setup({
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            mylauncher,
+            s.mytaglist,
+            s.mypromptbox,
+        },
+        s.mytasklist, -- Middle widget
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            awful.widget.keyboardlayout(),
+            wibox.widget.systray(),
+            wibox.widget.textclock(),
+            s.mylayoutbox,
+        },
+    })
+end
+
+return {
+    on_connect = on_connect,
+}
