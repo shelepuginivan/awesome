9654624d2a565ba176b947ac1b3dd172f9e917a2
shelepuginivan <shelepuginivanm@gmail.com>
2023-12-02 00:56:29 +0300

    feat(bindings): separate client buttons bindings

--- rc.lua
+++ rc.lua
@@ -248,20 +248,6 @@ for i = 1, 9 do
     )
 end
 
-clientbuttons = gears.table.join(
-    awful.button({}, 1, function(c)
-        c:emit_signal('request::activate', 'mouse_click', { raise = true })
-    end),
-    awful.button({ modkey }, 1, function(c)
-        c:emit_signal('request::activate', 'mouse_click', { raise = true })
-        awful.mouse.client.move(c)
-    end),
-    awful.button({ modkey }, 3, function(c)
-        c:emit_signal('request::activate', 'mouse_click', { raise = true })
-        awful.mouse.client.resize(c)
-    end)
-)
-
 -- Set keys
 root.keys(globalkeys)
 -- }}}
