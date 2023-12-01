82a57099a75194064ebf40421efe227c490fcf79
shelepuginivan <shelepuginivanm@gmail.com>
2023-12-01 23:00:03 +0300

    feat(core): separate menu module

---
+++ core/menu.lua
@@ -0,0 +1,56 @@
+local awful = require('awful')
+local beautiful = require('beautiful')
+local hotkeys_popup = require('awful.hotkeys_popup')
+
+-- Load Debian menu entries
+local debian = require('debian.menu')
+local has_fdo, freedesktop = pcall(require, 'freedesktop')
+
+local vars = require('core.variables')
+
+local terminal = vars.terminal
+local editor = vars.editor
+local editor_cmd = terminal .. ' -e ' .. editor
+
+-- Create a launcher widget and a main menu
+local awesome_menu = {
+    {
+        'hotkeys',
+        function()
+            hotkeys_popup.show_help(nil, awful.screen.focused())
+        end,
+    },
+    { 'manual', terminal .. ' -e man awesome' },
+    { 'edit config', editor_cmd .. ' ' .. awesome.conffile },
+    { 'restart', awesome.restart },
+    {
+        'quit',
+        function()
+            awesome.quit()
+        end,
+    },
+}
+
+local menu_awesome = { 'Awesome', awesome_menu, beautiful.awesome_icon }
+local menu_terminal = { 'Open terminal', terminal }
+
+local menu_main = awful.menu({
+    items = {
+        menu_awesome,
+        { 'Debian', debian.menu.Debian_menu.Debian },
+        menu_terminal,
+    },
+})
+
+if has_fdo then
+    menu_main = freedesktop.menu.build({
+        before = { menu_awesome },
+        after = { menu_terminal },
+    })
+end
+
+return {
+    awesome = menu_awesome,
+    main = menu_main,
+    terminal = menu_terminal,
+}
--- rc.lua
+++ rc.lua
@@ -17,9 +17,8 @@ local hotkeys_popup = require('awful.hotkeys_popup')
 -- when client with a matching name is opened:
 require('awful.hotkeys_popup.keys')
 
--- Load Debian menu entries
-local debian = require('debian.menu')
-local has_fdo, freedesktop = pcall(require, 'freedesktop')
+-- Menu
+local menu = require('core.menu')
 
 -- Error handling
 require('core.error_handling')
@@ -42,44 +41,7 @@ awful.layout.layouts = require('core.layouts')
 -- }}}
 
 -- {{{ Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
-    {
-        'hotkeys',
-        function()
-            hotkeys_popup.show_help(nil, awful.screen.focused())
-        end,
-    },
-    { 'manual', terminal .. ' -e man awesome' },
-    { 'edit config', editor_cmd .. ' ' .. awesome.conffile },
-    { 'restart', awesome.restart },
-    {
-        'quit',
-        function()
-            awesome.quit()
-        end,
-    },
-}
-
-local menu_awesome = { 'awesome', myawesomemenu, beautiful.awesome_icon }
-local menu_terminal = { 'open terminal', terminal }
-
-if has_fdo then
-    mymainmenu = freedesktop.menu.build({
-        before = { menu_awesome },
-        after = { menu_terminal },
-    })
-else
-    mymainmenu = awful.menu({
-        items = {
-            menu_awesome,
-            { 'Debian', debian.menu.Debian_menu.Debian },
-            menu_terminal,
-        },
-    })
-end
-
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = mymainmenu })
+mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = menu.main })
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
@@ -218,7 +180,7 @@ end)
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
     awful.button({}, 3, function()
-        mymainmenu:toggle()
+        menu.main:toggle()
     end),
     awful.button({}, 4, awful.tag.viewnext),
     awful.button({}, 5, awful.tag.viewprev)
@@ -239,7 +201,7 @@ globalkeys = gears.table.join(
         awful.client.focus.byidx(-1)
     end, { description = 'focus previous by index', group = 'client' }),
     awful.key({ modkey }, 'w', function()
-        mymainmenu:show()
+        menu.main:show()
     end, { description = 'show main menu', group = 'awesome' }),
 
     -- Layout manipulation
