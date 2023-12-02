2bb148c951685a9300a3833e5f1c7a1f549a402b
shelepuginivan <shelepuginivanm@gmail.com>
2023-12-02 20:05:46 +0300

    feat(theme): add colorschemes

---
+++ theme/colorschemes.lua
@@ -0,0 +1,38 @@
+local default = {
+    bg0 = '#222222',
+    bg1 = '#535d6c',
+    bg2 = '#444444',
+    fg0 = '#ffffff',
+    fg1 = '#aaaaaa',
+    red = '#ff0000',
+    dark_red = '#91231c',
+}
+
+local gruvbox = {
+    bg0 = '#282828',
+    bg1 = '#3c3836',
+    bg2 = '#504945',
+    fg0 = '#fbf1c7',
+    fg1 = '#ebdbb2',
+    red = '#fb4934',
+    dark_red = '#cc241d',
+    green = '#b8bb26',
+    dark_green = '#98971a',
+    yellow = '#fabd2f',
+    dark_yellow = '#d79921',
+    orange = '#fe8019',
+    dark_orange = '#d65d0e',
+    blue = '#83a598',
+    dark_blue = '#458588',
+    purple = '#d3869b',
+    dark_purple = '#b16286',
+    aqua = '#8ec07c',
+    dark_aqua = '#689d6a',
+    gray = '#a89984',
+    dark_gray = '#928374',
+}
+
+return {
+    default = default,
+    gruvbox = gruvbox,
+}
